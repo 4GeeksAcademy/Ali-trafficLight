49ef751243ab76405286b0ea369ca8ee8bbc62a3
Alimilan88 <162614275+Alimilan88@users.noreply.github.com>
2024-06-15 11:49:22 +0000

    listo

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -8,24 +8,23 @@ export const TrafficLight = () => {
         <div className='all'>
             <div className="container">
                 <div
-                    className={`circle col-2 bg-success ${color === 'verde' ? 'glow-green' : ''}`}
+                    className={`circle bg-success ${color === 'verde' ? 'glow-green' : ''}`}
                     onClick={() => setColor('verde')}
                 >
                 </div>
                 <div
-                    className={`circle col-2 bg-warning ${color === 'amarillo' ? 'glow-yellow' : ''}`}
+                    className={`circle bg-warning ${color === 'amarillo' ? 'glow-yellow' : ''}`}
                     onClick={() => setColor('amarillo')}
                 >
                 </div>
                 <div
-                    className={`circle col-2 bg-danger ${color === 'rojo' ? 'glow-red' : ''}`}
+                    className={`circle bg-danger ${color === 'rojo' ? 'glow-red' : ''}`}
                     onClick={() => setColor('rojo')}
                 >
                 </div>
             </div>
             <div className='pillar'>
-                <button className={`pressToCross col-2`}
-                    onClick={() => setColor('rojo')}></button>
+                <button className={`pressToCross col-2`} onClick={() => setColor('rojo')}></button>
                 <p className='text'>Press to Cross</p>
             </div>
         </div>
